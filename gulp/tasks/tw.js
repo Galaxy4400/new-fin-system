1ab9187a7903e80e53a70a6b40de318513153f06
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-18 02:54:45 +0300

    commit

--- gulp/tasks/tw.js
+++ gulp/tasks/tw.js
@@ -1,9 +1,8 @@
-import { exec } from "child_process";
-import { join } from "path";
+import { spawn } from "child_process";
 
 export const tailwind = (done) => {
-	const input = join(app.path.src.css);
-	const output = join(app.path.build.css, "tailwind.css");
+	const input = app.path.src.css;
+	const output = app.path.build.css + "tailwind.css";
 
 	const args = ["-i", input, "-o", output];
 
@@ -16,56 +15,6 @@ export const tailwind = (done) => {
 		stdio: "inherit",
 	});
 
-	if (app.isBuild) {
-		tw.on("close", () => {
-			done();
-		});
-	}
-
-	if (app.isDev) {
-		done();
-	}
+	if (app.isBuild) tw.on("close", done);
+	if (app.isDev) done();
 };
-
-// export const tailwind = (done) => {
-// 	const input = join(app.path.src.css);
-// 	const output = join(app.path.build.css, "tailwind.css");
-
-// 	const args = ["-i", input, "-o", output];
-
-// 	if (app.isDev) {
-// 		args.push("--watch");
-// 	}
-
-// 	const tw = spawn("npx", ["tailwindcss", ...args], {
-// 		shell: true,
-// 		stdio: "inherit",
-// 	});
-
-// 	if (app.isBuild) {
-// 		tw.on("close", () => done());
-// 	} else {
-// 		done();
-// 	}
-// };
-
-// export const tailwind = (done) => {
-// 	const input = join(app.path.src.css);
-// 	const output = join(app.path.build.css, "tailwind.css");
-
-// 	if (app.isBuild) {
-// 		exec(`npx tailwindcss -i ${input} -o ${output}`, (err, stdout, stderr) => {
-// 			if (err) console.log(err);
-// 			if (stdout) console.log(stdout);
-// 			if (stderr) console.log(stderr);
-// 			done();
-// 		});
-// 	}
-
-// 	if (app.isDev) {
-// 		exec(`npx tailwindcss -i ${input} -o ${output} --watch`, (err) => {
-// 			if (err) console.log(err);
-// 		});
-// 		done();
-// 	}
-// };
