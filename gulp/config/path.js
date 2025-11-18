b13e75391c84c25bc7420be1767618d9669fbd1a
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-18 14:48:09 +0300

    commit

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,25 +1,24 @@
 import * as nodePath from "path";
 
 const rootFolder = nodePath.basename(nodePath.resolve());
-const buildFolder = "./dist";
+const buildFolder = "./dist/assets";
 const srcFolder = "./src";
 
 export const path = {
 	build: {
-		js: `${buildFolder}/assets/js/`,
-		css: `${buildFolder}/assets/css/`,
-		img: `${buildFolder}/assets/img/`,
-		fonts: `${buildFolder}/assets/fonts/`,
+		js: `${buildFolder}/js/`,
+		css: `${buildFolder}/css/`,
+		img: `${buildFolder}/img/`,
 	},
 	src: {
 		js: `${srcFolder}/js/*.js`,
-		img: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,avif,svg}`,
+		img: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp,avif}`,
 		css: `${srcFolder}/css/tailwind.css`,
 	},
 	watch: {
 		js: `${srcFolder}/js/**/*.js `,
 		css: `${srcFolder}/css/tailwind.css`,
-		img: `${srcFolder}/assets/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp,avif}`,
+		img: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp,avif}`,
 	},
 	clean: buildFolder,
 	buildFolder: buildFolder,
---
+++ gulpfile.copy.js
@@ -0,0 +1,51 @@
+import gulp from "gulp"; // Основной модуль
+import { path } from "./gulp/config/path.js"; // Импорт путей
+import { plugins } from "./gulp/config/plugins.js"; // Импорт общих плагинов
+
+// Передаем значения в глобальную переменную и добавляем комментарий для eslint:
+/* global global process*/
+global.app = {
+	isBuild: process.argv.includes("--build"),
+	isDev: !process.argv.includes("--build"),
+	path: path,
+	gulp: gulp,
+	plugins: plugins,
+};
+
+// Импорт задач
+import { reset } from "./gulp/tasks/reset.js";
+// import { js } from "./gulp/tasks/js.js";
+// import { images } from "./gulp/tasks/images.js";
+import { tailwind } from "./gulp/tasks/tw.js";
+// import { copy } from "./gulp/tasks/copy.js";
+// import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+// import { scss } from "./gulp/tasks/scss.js";
+
+// Наблюдатель за изменениями в файлах
+function watcher() {
+	// gulp.watch(path.watch.files, copy);
+	// gulp.watch(path.watch.html, html);
+	// gulp.watch(path.watch.scss, scss);
+	// gulp.watch(path.watch.js, js);
+	// gulp.watch(path.watch.images, images);
+}
+
+// Последовательная обработка шрифтов
+// const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle);
+
+// Основные задачи
+// const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(gulp.parallel(tailwind));
+
+// Построение сценариев выполнения задач
+const build = gulp.series(reset, mainTasks);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server, tailwind));
+
+// Экспорт сценариев для добавления в скрипт в package.json
+export { dev };
+export { build };
+// Вызов сценария командой npm run dev или npm run build
+
+// Выполнение сценария по умолчанию
+gulp.task("default", dev);
