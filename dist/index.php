5649e69a41718e7e29a7d81b88e3362ff24b498b
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-18 23:46:00 +0300

    commit

--- dist/index.php
+++ dist/index.php
@@ -4,19 +4,17 @@ include_once 'engine/engine.php';
 
 <!DOCTYPE html>
 <html lang="<?= $lang ?>">
+  <?php include 'components/layout/head.php' ?>
 
-<?php include 'components/layout/head.php' ?>
+  <body class="flex min-h-screen flex-col">
+    <?php include 'components/layout/header.php' ?>
 
-<body>
-	<div>
-		<?php include 'components/layout/header.php' ?>
+    <main class="grow">
+      <div class="max-w-container bs-content mx-auto px-4">
+        Loremx ipsum dolor sit amet consectetur adipisicing elit.
+      </div>
+    </main>
 
-		<main class="text-primary">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit.
-		</main>
-
-		<?php include 'components/layout/footer.php' ?>
-	</div>
-</body>
-
+    <?php include 'components/layout/footer.php' ?>
+  </body>
 </html>
