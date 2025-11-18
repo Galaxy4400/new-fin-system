4fe0a2d118808a5a547d31f82107bdbcb70af486
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-19 01:24:09 +0300

    commit

--- dist/components/layout/scripts.php
+++ dist/components/layout/scripts.php
@@ -1,18 +1,17 @@
-<script src="/public/js/localization.js" type="module"></script>
+<!-- <script src="/public/js/localization.js" type="module"></script>
 <script src="/public/js/currency.js" type="module"></script>
 <script src="/public/js/formsPlugin.js" type="module"></script>
 
-<!-- libs -->
 <script src="/public/js/lazyload.min.js" defer></script>
 
-<!-- plugins -->
 <script src="/public/js/spoiler.js" defer></script>
 
-<!-- files -->
 <script src="/public/js/langSelect.js" defer></script>
 <script src="/public/js/cookies.js" defer></script>
 <script src="/public/js/functions.js" defer></script>
 <script src="/public/js/scroll.js" defer></script>
 <script src="/public/js/formsHandlers.js" defer></script>
 <script src="/public/js/modals.js" defer></script>
-<script src="/public/js/scripts.js" defer></script>
+<script src="/public/js/scripts.js" defer></script> -->
+
+<script src="/assets/js/cdn.min.js" defer></script>
