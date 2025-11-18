266493fc10cd8f5dec6a40f66dbc9226a374dc4b
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-19 00:58:29 +0300

    commit

--- dist/components/layout/header.php
+++ dist/components/layout/header.php
@@ -1,23 +1,21 @@
-<header class="py-5 shadow">
+<header class="bg-white py-5 shadow">
   <div class="max-w-container bs-content mx-auto px-4">
-    <div class="flex justify-between">
-      <a class="uppercase" href="<?= url() ?>">
+    <div class="flex items-center justify-between">
+      <a class="font-medium uppercase" href="<?= url() ?>">
         <div><?= $offer_name ?></div>
       </a>
       <nav class="flex grow justify-center">
         <ul class="flex flex-wrap justify-center gap-x-6 gap-y-3 md:gap-x-10 lg:gap-x-14">
-          <li><a href="<?= url('/', '', '#about') ?>">Item 1</a></li>
-          <li><a href="<?= url('/', '', '#features') ?>">Item 2</a></li>
-          <li><a href="<?= url('/', '', '#reviews') ?>">Item 3</a></li>
-          <li><a href="<?= url('/', '', '#faq') ?>">Item 4</a></li>
+          <li><a class="hover:text-primary transition-colors" href="<?= url('/', '', '#about') ?>">About</a></li>
+          <li><a class="hover:text-primary transition-colors" href="<?= url('/', '', '#features') ?>">Features</a></li>
+          <li><a class="hover:text-primary transition-colors" href="<?= url('/', '', '#reviews') ?>">Reviews</a></li>
+          <li><a class="hover:text-primary transition-colors" href="<?= url('/', '', '#faq') ?>">FAQ</a></li>
         </ul>
       </nav>
-      <div class="flex gap-4">
+      <div class="flex items-center gap-4">
+        <a class="btn" href="<?= url('sign-up') ?>">Sign up</a>
         <div>
-          <a href="<?= url('sign-up') ?>">Sign up</a>
-        </div>
-        <div>
-          <button><?= $lang ?></button>
+          <button class="btn"><?= $lang ?></button>
           <!-- <div>
             <div>
               <ul>
