92571749a4fdebf865949cbcde6cd2c7b414ce4d
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-19 03:28:04 +0300

    commit

--- dist/components/layout/header.php
+++ dist/components/layout/header.php
@@ -1,11 +1,11 @@
 <header class="bg-white py-5 shadow">
   <div class="max-w-container bs-content mx-auto px-4">
-    <div class="flex items-center justify-between">
+    <div class="flex items-center justify-between gap-8">
       <a class="font-medium uppercase" href="<?= url() ?>">
         <div><?= $offer_name ?></div>
       </a>
       <nav class="flex grow justify-center">
-        <ul class="flex flex-wrap justify-center gap-x-6 gap-y-3 md:gap-x-10 lg:gap-x-14">
+        <ul class="flex flex-wrap justify-center gap-x-6 gap-y-2 md:gap-x-10 lg:gap-x-14">
           <li><a class="hover:text-primary transition-colors" href="<?= url('/', '', '#about') ?>">About</a></li>
           <li><a class="hover:text-primary transition-colors" href="<?= url('/', '', '#features') ?>">Features</a></li>
           <li><a class="hover:text-primary transition-colors" href="<?= url('/', '', '#reviews') ?>">Reviews</a></li>
@@ -13,9 +13,9 @@
         </ul>
       </nav>
       <div class="flex items-center gap-4">
-        <a class="btn" href="<?= url('sign-up') ?>">Sign up</a>
+        <a class="btn-primary" href="<?= url('sign-up') ?>">Sign up</a>
         <div class="relative" x-data="{ open: false }" @click.outside="open = false">
-          <button class="btn" @click="open = !open"><?= $lang ?></button>
+          <button class="btn-primary" @click="open = !open"><?= $lang ?></button>
           <div
             class="bg-primary absolute top-[calc(100%+10px)] left-0 max-h-[150px] w-[60px] overflow-y-auto rounded-[10px] text-white transition-all duration-300"
             x-bind:class="open ? 'opacity-100 pointer-events-auto translate-y-0' : 'opacity-0 pointer-events-none translate-y-3'"
