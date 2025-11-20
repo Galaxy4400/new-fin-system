d67f57b48d6082f5bb3ac916b9b486405f175791
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-20 21:26:40 +0300

    commit

--- dist/components/layout/head-elements/schema.php
+++ dist/components/layout/head-elements/schema.php
@@ -2,11 +2,11 @@
   {
   	"@context": "https://schema.org/",
   	"@type": "SoftwareApplication",
-  	"name": "<?= htmlspecialchars($offer_name) ?>",
-  	"url": "<?= htmlspecialchars($currentPageUrl) ?>",
+  	"name": "<?= $offer_name ?>",
+  	"url": "<?= getCurrentPageUrl() ?>",
   	"logo": "https://<?= $domain ?>/assets/img/favicon.ico",
   	"image": "https://<?= $domain ?>/assets/img/twitter_image.png",
-  	"description": "<?= htmlspecialchars(t('t.main.meta_description', ['{site_name}' => $offer_name])) ?>",
+  	"description": "<?= t('t.main.meta_description', ['{site_name}' => $offer_name]) ?>",
   	"applicationCategory": "FinanceApplication",
   	"operatingSystem": "Web Browser",
   	"aggregateRating": {
@@ -25,7 +25,7 @@
   	},
   	"author": {
   		"@type": "Brand",
-  		"name": "<?= htmlspecialchars($offer_name) ?>"
+  		"name": "<?= $offer_name ?>"
   	}
   }
 </script>
