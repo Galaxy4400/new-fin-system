d76e4c7956e2b8d88a5add2be372f15fa9a1b85f
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-21 12:00:57 +0300

    commit

--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-	app.plugins.browsersync.init({
-		proxy: "http://fin_system_2.local:8080", // Apache virtual host
-		notify: false,
-		port: 3000,
-		open: true, // можно false, если не хочешь автооткрытие
-	});
-	done();
-};
