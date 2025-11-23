7b56ff002db853a58303e8aaa1b2ccae7a817e47
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-23 14:07:30 +0300

    commit

--- dist/components/layout/head-elements/scripts.php
+++ dist/components/layout/head-elements/scripts.php
@@ -4,6 +4,8 @@
   window.defaultLang = '<?= $defaultLang ?>';
 </script>
 
+<link rel="preload" href="https://cdn.jsdelivr.net/npm/intl-tel-input@25.12.5/build/js/utils.min.js" as="script" />
+
 <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@25.12.5/build/js/intlTelInput.min.js" defer></script>
 
 <script src="/assets/js/localization.js" type="module"></script>
