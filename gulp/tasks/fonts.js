fd79f680d51c2028b1792509a40fe51a357b9ddb
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-24 00:26:54 +0300

    commit

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,20 +1,25 @@
 import gulp from 'gulp';
-import fonter from 'gulp-fonter';
 import ttf2woff from 'gulp-ttf2woff';
 import ttf2woff2 from 'gulp-ttf2woff2';
 import merge from 'merge-stream';
 
 export const fonts = () => {
-  const convert = gulp
-    .src(app.path.src.fonts)
-    .pipe(fonter({ formats: ['ttf'] }))
-    .pipe(gulp.dest(app.path.build.fonts))
+  // TTF → WOFF
+  const ttfToWoff = gulp
+    .src(app.path.src.fonts) // *.ttf
     .pipe(ttf2woff())
     .pipe(gulp.dest(app.path.build.fonts));
 
-  const woff2 = gulp.src(app.path.src.fonts).pipe(ttf2woff2()).pipe(gulp.dest(app.path.build.fonts));
+  // TTF → WOFF2
+  const ttfToWoff2 = gulp
+    .src(app.path.src.fonts) // *.ttf
+    .pipe(ttf2woff2())
+    .pipe(gulp.dest(app.path.build.fonts));
 
-  const copyReady = gulp.src(app.path.src.fontsw).pipe(gulp.dest(app.path.build.fonts));
+  // Копируем готовые WOFF/WOFF2
+  const copyReady = gulp
+    .src(app.path.src.fontsw) // *.{woff,woff2}
+    .pipe(gulp.dest(app.path.build.fonts));
 
-  return merge(convert, woff2, copyReady);
+  return merge(ttfToWoff, ttfToWoff2, copyReady);
 };
