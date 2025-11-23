dfe41fb611d8b481c5a51c57d443559f21b9fe85
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-23 13:44:05 +0300

    commit

--- dist/components/layout/head-elements/scripts.php
+++ dist/components/layout/head-elements/scripts.php
@@ -4,25 +4,9 @@
   window.defaultLang = '<?= $defaultLang ?>';
 </script>
 
-<script>
-  async function loadFormPlugins() {
-    await Promise.all([
-      import('https://cdn.jsdelivr.net/npm/intl-tel-input@25.12.5/build/js/intlTelInput.min.js'),
-      import('https://cdn.jsdelivr.net/npm/intl-tel-input@25.12.5/build/js/utils.min.js'),
-    ]);
-
-    await import('/assets/js/forms.js');
-  }
-
-  document.addEventListener('DOMContentLoaded', () => {
-    if ('requestIdleCallback' in window) {
-      requestIdleCallback(loadFormPlugins, { timeout: 3000 });
-    } else {
-      setTimeout(loadFormPlugins, 2000);
-    }
-  });
-</script>
+<script src="https://cdn.jsdelivr.net/npm/intl-tel-input@25.12.5/build/js/intlTelInput.min.js" defer></script>
 
 <script src="/assets/js/localization.js" type="module"></script>
 <script src="/assets/js/currency.js" type="module"></script>
+<script src="/assets/js/forms.js" type="module"></script>
 <script src="/assets/js/scripts.js" type="module"></script>
