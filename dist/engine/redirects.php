37adddaa9e055d69728ccf01554021987928c67f
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-25 14:35:26 +0300

    commit

--- dist/engine/redirects.php
+++ dist/engine/redirects.php
@@ -1,19 +1,21 @@
 <?php
 
+// Удаление лишних слэшей в URL
 $normalized = preg_replace('#/+#', '/', $_SERVER['REQUEST_URI']);
 if ($_SERVER['REQUEST_URI'] !== $normalized) {
 	header("Location: $normalized", true, 301);
 	exit;
 }
 
+// Удаление языка из URL
 $segments = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-
 if (isset($segments[1]) && in_array($segments[1], $supportedLanguages)) {
 	$cleaned = '/' . $segments[0] . '/' . implode('/', array_slice($segments, 2));
 	header("Location: $cleaned", true, 301);
 	exit;
 }
 
+// Приведение URL к нижнему регистру
 if (preg_match('/[A-Z]/', $_SERVER['REQUEST_URI'])) {
 	$lowerUri = strtolower($_SERVER['REQUEST_URI']);
 	$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
@@ -22,6 +24,7 @@ if (preg_match('/[A-Z]/', $_SERVER['REQUEST_URI'])) {
 	exit;
 }
 
+// Обработка действий через параметр GET
 if (isset($_GET['action']) && $_GET['action'] === 'send') {
 	require __DIR__ . '/send.php';
 	exit;
