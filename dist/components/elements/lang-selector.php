beba87955bf5eda23751a295fa5eed36159e39c8
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-26 01:15:22 +0300

    commit

--- dist/components/elements/lang-selector.php
+++ dist/components/elements/lang-selector.php
@@ -1,7 +1,7 @@
 <div class="relative" data-connect-parent>
   <button class="lang-icon group" data-connector="lang-menu">
     <div class="aspect-25/13 min-w-[25px] overflow-hidden rounded-sm">
-      <img src="<?= flagUrl($currentLang) ?>" alt="<?= $currentLang ?>" width="25" height="20" />
+      <img src="<?= flagUrl($currentLang) ?>" alt="<?= $currentLang ?>" width="25" height="25" />
     </div>
     <span class="uppercase"><?= $currentLang ?></span>
     <div class="min-w-5 transition-transform duration-300 group-data-active:rotate-180">
@@ -17,7 +17,7 @@
           href="<?= getLocalizedUrl($listLang) ?>"
         >
           <div class="overflow-hidden rounded-sm">
-            <img data-src="<?= flagUrl($listLang) ?>" alt="<?= $listLang ?>" width="25" height="20" data-flag-img />
+            <img data-src="<?= flagUrl($listLang) ?>" alt="<?= $listLang ?>" width="25" height="25" data-flag-img />
           </div>
           <span class="uppercase"><?= $listLang ?></span>
         </a>
