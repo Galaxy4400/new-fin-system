3e6467843754817d10bfd78c2fa005657376b024
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-25 15:08:20 +0300

    commit

--- dist/components/forms/main.php
+++ dist/components/forms/main.php
@@ -55,7 +55,15 @@
       </label>
       <label class="grid gap-1.5">
         <span class="label"><?= t('t.main.form_phone_label') ?></span>
-        <input class="input" type="tel" name="phone" data-phone required data-should-validate />
+        <input
+          class="input"
+          type="tel"
+          name="phone"
+          placeholder="<?= t('t.main.form_phone_placeholder') ?>"
+          data-phone
+          required
+          data-should-validate
+        />
       </label>
     </div>
     <div>
