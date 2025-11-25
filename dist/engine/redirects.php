1c2f665d8f6761aa2651c6f49f7f6ade73c67167
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-26 01:21:45 +0300

    commit

--- dist/engine/redirects.php
+++ dist/engine/redirects.php
@@ -10,26 +10,14 @@ if ($uri == 'send') {
 // Удаление лишних слэшей в URL
 $normalized = preg_replace('#/+#', '/', $_SERVER['REQUEST_URI']);
 if ($_SERVER['REQUEST_URI'] !== $normalized) {
-	header("Location: $normalized", true, 302);
+	header("Location: $normalized", true, 301);
 	exit;
 }
-
-// Удаление языка из URL
-// $segments = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-// if (isset($segments[0]) && in_array($segments[0], $supportedLanguages)) {
-// 	$cleaned = '/' . implode('/', array_slice($segments, 1));
-// 	if ($cleaned === '/') {
-// 		header("Location: /", true, 302);
-// 	} else {
-// 		header("Location: $cleaned", true, 302);
-// 	}
-// 	exit;
-// }
 
 // Приведение URL к нижнему регистру
 if (preg_match('/[A-Z]/', $_SERVER['REQUEST_URI'])) {
 	$lowerUri = strtolower($_SERVER['REQUEST_URI']);
 	$host = $_SERVER['HTTP_HOST'];
-	header("Location: {$protocolType}://{$host}{$lowerUri}", true, 302);
+	header("Location: {$protocolType}://{$host}{$lowerUri}", true, 301);
 	exit;
 }
