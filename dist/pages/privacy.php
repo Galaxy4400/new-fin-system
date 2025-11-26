0d2d511d72d2b4e13a8ab6594a0874b7f48e2e85
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-26 23:36:37 +0300

    commit

--- dist/pages/privacy.php
+++ dist/pages/privacy.php
@@ -144,139 +144,69 @@
       </div>
 
       <div class="border-primary rounded-2xl md:border md:p-8">
-        <div class="mb-7 flex grid-cols-3 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="text-primary text-base">Scope</div>
-          <div class="text-primary text-base">Legal Basis</div>
+        <div class="mb-2 flex grid-cols-3 gap-6 md:mb-5 md:grid md:grid-cols-[3fr_2fr]">
+          <p class="text-primary text-base">Scope</p>
+          <p class="text-primary text-base">Legal Basis</p>
         </div>
-        <div class="border-primary gap-6 border-b py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="1."
-            >
-              <p class="h3">
-                To provide your personal information to third-party parties on your request to gain access to digital
-                trading<br />
-                If you ask for it We may request Personal Data from you to forward it to third-party companies.
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>You have given your consent to processing of your Personal Data for one or multiple reasons.</p>
-          </div>
+        <div class="border-primary grid gap-x-6 gap-y-2 border-b py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">
+            1. To provide your personal information to third-party parties on your request to gain access to digital
+            trading<br />
+            If you ask for it We may request Personal Data from you to forward it to third-party companies.
+          </p>
+          <p>You have given your consent to processing of your Personal Data for one or multiple reasons.</p>
         </div>
-        <div class="border-primary gap-6 border-b py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="2."
-            >
-              <p class="h3">
-                To answer your requests, questions or concerns<br />
-                Personal Data is necessary to help you answer any questions you might have regarding the Services.
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
-          </div>
-        </div>
-        <div class="border-primary gap-6 border-b py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="3."
-            >
-              <p class="h3">
-                To meet any legal obligation or administrative, judicial or legal or judicial Personal Data is processed
-                to meet the legal requirements.
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>To ensure compliance with legal obligations, processing is required.</p>
-          </div>
-        </div>
-        <div class="border-primary gap-6 border-b py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="4."
-            >
-              <p class="h3">
-                To enhance Our Services Personal<br />
-                Data may be used by us to enhance Our Services. This includes, among other things, any crashes or
-                malfunction reports that we collect in relation to the Services.
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
-          </div>
-        </div>
-        <div class="border-primary gap-6 border-b py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="5."
-            >
-              <p class="h3">To stop fraud and misuse of Our Services</p>
-            </div>
-          </div>
+        <div class="border-primary grid gap-x-6 gap-y-2 border-b py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">
+            2. To answer your requests, questions or concerns<br />
+            Personal Data is necessary to help you answer any questions you might have regarding the Services.
+          </p>
           <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
         </div>
-        <div class="border-primary gap-6 border-b py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="6."
-            >
-              <p class="h3">
-                To perform and manage actions that are in line with the requirements of Our Services<br />
-                Such activities include back-office functions, business development activities, strategic
-                decision-making, oversight mechanisms, etc.
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
-          </div>
+        <div class="border-primary grid gap-x-6 gap-y-2 border-b py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">
+            3. To meet any legal obligation or administrative, judicial or legal or judicial Personal Data is processed
+            to meet the legal requirements.
+          </p>
+          <p>To ensure compliance with legal obligations, processing is required.</p>
         </div>
-        <div class="border-primary gap-6 border-b py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="7."
-            >
-              <p class="h3">
-                To conduct analysis, such as statistical analysis<br />
-                To take decisions on different issues, we employ different analytical techniques (including those that
-                are statistical).
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
-          </div>
+        <div class="border-primary grid gap-x-6 gap-y-2 border-b py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">
+            4. To enhance Our Services Personal<br />
+            Data may be used by us to enhance Our Services. This includes, among other things, any crashes or
+            malfunction reports that we collect in relation to the Services.
+          </p>
+          <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
         </div>
-        <div class="gap-6 py-4 md:grid md:grid-cols-[3fr_2fr]">
-          <div class="">
-            <div
-              class="relative pl-6 before:absolute before:top-0 before:left-0 before:content-[attr(data-num)]"
-              data-num="8."
-            >
-              <p class="h3">
-                To safeguard our assets, rights and interests as well as those of third party We have developed HTML0 to
-                establish and defend legal claims<br />
-                Personal Data may be processed by us in order to safeguard our rights, interests and assets, or those of
-                a third parties, in compliance to any applicable laws, regulations or agreements and any conditions,
-                terms or policies.
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
-          </div>
+        <div class="border-primary grid gap-x-6 gap-y-2 border-b py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">5. To stop fraud and misuse of Our Services</p>
+          <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
+        </div>
+        <div class="border-primary grid gap-x-6 gap-y-2 border-b py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">
+            6. To perform and manage actions that are in line with the requirements of Our Services<br />
+            Such activities include back-office functions, business development activities, strategic decision-making,
+            oversight mechanisms, etc.
+          </p>
+          <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
+        </div>
+        <div class="border-primary grid gap-x-6 gap-y-2 border-b py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">
+            7. To conduct analysis, such as statistical analysis<br />
+            To take decisions on different issues, we employ different analytical techniques (including those that are
+            statistical).
+          </p>
+          <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
+        </div>
+        <div class="grid gap-x-6 gap-y-2 py-4 md:grid-cols-[3fr_2fr]">
+          <p class="h3">
+            8. To safeguard our assets, rights and interests as well as those of third party We have developed HTML0 to
+            establish and defend legal claims<br />
+            Personal Data may be processed by us in order to safeguard our rights, interests and assets, or those of a
+            third parties, in compliance to any applicable laws, regulations or agreements and any conditions, terms or
+            policies.
+          </p>
+          <p>The legitimate interests of the Company or the interests of a third party need to be processed.</p>
         </div>
       </div>
 
@@ -320,7 +250,96 @@
         <p class="text-2xl font-medium">Types of Cookies:</p>
         <p>They may be used by us based on their purpose:</p>
       </div>
-      <div>table</div>
+
+      <div class="border-primary max-md:grid max-md:gap-1 md:rounded-2xl md:border md:p-6">
+        <!-- row 1 -->
+        <div
+          class="border-primary grid items-start gap-2 py-2 md:grid-cols-[15fr_50fr_35fr] md:gap-6 md:border-b md:py-4"
+        >
+          <!-- col 1 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:min-h-12">The type of cookie</p>
+            <p>Cookies are absolutely necessary</p>
+          </div>
+
+          <!-- col 2 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:min-h-12">Scope</p>
+            <p>
+              These cookies are necessary for you to be able to access the features you've asked for and to navigate our
+              website. Cookies are used to provide the information, products and services that you have asked us for.<br />
+              They are required for your device to download and stream data. This lets you navigate around the website,
+              utilize its features, and then return to pages you've previously been to.
+            </p>
+          </div>
+
+          <!-- col 3 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:min-h-12">Additional Information</p>
+            <p>
+              Cookies collect Personal Data, such as your username, as well as the last login date to confirm that you
+              are logged in to the site. They are erased once you close your web browser (session Cookies).
+            </p>
+          </div>
+        </div>
+
+        <!-- row 2 -->
+        <div
+          class="border-primary grid items-start gap-2 py-2 md:grid-cols-[15fr_50fr_35fr] md:gap-6 md:border-b md:py-4"
+        >
+          <!-- col 1 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:hidden">The type of cookie</p>
+            <p>Functionality Cookies</p>
+          </div>
+
+          <!-- col 2 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:hidden">Scope</p>
+            <p>
+              Cookies allow us to recognize you each time you visit our site and also to save your preferences and
+              preferences.
+            </p>
+          </div>
+
+          <!-- col 3 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:hidden">Additional Information</p>
+            <p>
+              Cookies collect Personal Data, such as your username, as well as the last login date to confirm that you
+              are logged in to the site. They are erased once you close your web browser (session Cookies).
+            </p>
+          </div>
+        </div>
+
+        <!-- row 3 -->
+        <div class="grid items-start gap-6 py-4 md:grid-cols-[15fr_50fr_35fr]">
+          <!-- col 1 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:hidden">The type of cookie</p>
+            <p>Cookies for performance</p>
+          </div>
+
+          <!-- col 2 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:hidden">Scope</p>
+            <p>
+              Cookies are used to gather statistical data on the site's performance as well as to help enhance it. They
+              also permit Us to carry out analysis on our website.
+            </p>
+          </div>
+
+          <!-- col 3 -->
+          <div class="grid items-start gap-2 md:mb-0 md:gap-6">
+            <p class="text-primary text-base md:hidden">Additional Information</p>
+            <p>
+              Cookies store data that is anonymous that isn't associated with any identifiable or identified natural
+              person. They can be removed when you quit your browser. Other cookies have indefinite validity.
+            </p>
+          </div>
+        </div>
+      </div>
+
       <div class="grid gap-2 md:gap-4">
         <div>
           <p>
