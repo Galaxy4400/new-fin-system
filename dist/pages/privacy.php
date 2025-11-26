cda426cd77cf9fc08f22bc3c46120d63592d09f5
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-26 20:37:11 +0300

    commit

--- dist/pages/privacy.php
+++ dist/pages/privacy.php
@@ -1,26 +1,9 @@
 <div class="py-10 md:py-16">
   <div class="container-narrow grid gap-8 md:gap-12">
     <div class="">
-      <nav class="flex" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-1 md:space-x-3">
-          <li class="inline-flex items-center">
-            <a href="" class="inline-flex items-center text-base font-medium text-gray-900 hover:text-indigo-800">
-              Tab text
-            </a>
-          </li>
-          <li>
-            <div class="flex items-center">
-              -
-              <a href="" class="ml-1 text-base font-medium text-gray-900 hover:text-indigo-800 md:ml-2"> Tab text</a>
-            </div>
-          </li>
-          <li aria-current="page">
-            <div class="flex items-center">
-              -
-              <span class="ml-1 text-base font-medium text-indigo-600 md:ml-2">Tab text</span>
-            </div>
-          </li>
-        </ol>
+      <nav class="flex flex-wrap items-center text-sm text-gray-500 md:text-lg">
+        <a href="<?= url() ?>" class="breadcrumb-item"><?= t('t.main.menu_home') ?></a>
+        <span class="breadcrumb-item"><?= t('t.main.menu_privacy') ?></span>
       </nav>
     </div>
 
