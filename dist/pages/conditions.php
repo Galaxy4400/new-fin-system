03b135d5eeb350c2f16b7bfcf86f37dd6eae465d
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-11-27 00:53:02 +0300

    commit

--- dist/pages/conditions.php
+++ dist/pages/conditions.php
@@ -9,169 +9,42 @@
     </div>
     <div class="grid gap-6 md:gap-8">
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">1. General</h2>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_1') ?></h2>
         <p>
-          1.1. You are invited to visit the {site_name} ( "Website" )<br />
-          Our mail <a href="mailto:<?= getEmail() ?>"><?= getEmail() ?></a><br />
-          1.2. This website offers information on third-party platforms ( "Third-Party Platforms" ) to trade (the
-          "Services" )<br />
-          1.3. The Terms regulate Your ( "You" , "Your" , or "User" ) access to the Website and Services. Before you are
-          able to access the Services you must read these Terms carefully. These Terms are an legally binding agreement
-          between You and the owner of the website You, and the owner of the website. You must accept the Terms in all
-          respects if you do not wish to use the website. The Terms can be amended from time to time.<br />
-          These Terms contain our Privacy Policy. You accept these Terms and agree to our Privacy Policy. (You can read
-          our privacy policy here ).
+          <?= t('t.policy.conditions_2', ['{email}' => '<a href="mailto:' . getEmail() . '">'. getEmail() .'</a>']) ?>
         </p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">2. Eligibility</h2>
-        <p>
-          2.1. If you adhere to these terms and conditions The Website will be accessible to you.<br />
-          2.1.1. Minimum age 18 years old<br />
-          2.1.2. You can sign these Conditions and Terms.<br />
-          2.1.3. The laws in the country in which you live or use the Services are not in any way preventing users from
-          using the Website or any of its services.<br />
-          2.2. We do not make any guarantees, representations or warranties about the legality or usage of the services
-          or website for any person. We are not responsible for any illegal usage of the website or the services of any
-          user.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_3') ?></h2>
+        <p><?= t('t.policy.conditions_4') ?></p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">3. Territories with restricted access</h2>
-        <p>
-          3.1. Without limiting the scope of the information provided We reserve the right to restrict access to the
-          Services and/or Website (or any portion of them) to: (i), Users who reside in restricted areas (the
-          "Restricted Territories" ) and (ii) users we believe may be a regulatory, legal or reputational risk.<br />
-          3.2. We may also impose additional terms prior to accepting Users who are citizens of or of certain countries.
-          If users travel into restricted areas The Website or the Services may be temporarily unavailable or blocked.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_5') ?></h2>
+        <p><?= t('t.policy.conditions_6') ?></p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">4. Prohibited activities</h2>
-        <p>
-          4.1. You accept to utilize the Site and Services respectfully and not to:<br />
-          4.1.1. You are able to connect to our Website and use it to download, upload, share and publish, as well as to
-          transmit or send (a) information or other material that violates intellectual rights to privacy, property
-          rights and other rights; or (b) information that is not permitted to be published or distributed because of a
-          threat or injury caused by insult or defamation, slander, or racism, or (c) information that includes viruses
-          or other software that may harm our computer systems or those of third parties , or in a manner that blocks or
-          restricts other users from accessing the website. (d) any information or other material that is in violation
-          of any law. (e) information or materials that includes advertisements or other content that is not subject to
-          our prior written consent.<br />
-          4.1.2. Modify or delete any attributions, legal notices, labels or designations that are proprietary to this
-          Website.<br />
-          4.1.3. The access to the Services may be obtained through any interface that is not the Website.<br />
-          4.1.4. Do not interfere with the usage of the website or services provided by other users<br />
-          4.1.5. Bots and other automated techniques are used to gain access to the site and/or its services.<br />
-          4.1.6. You are not allowed to upload or transmit, or even attempt to upload, any content that is active or
-          passively involved in data collection or transmission mechanisms, such as web bugs, cookies, or spyware
-          devices, without our express permission.<br />
-          4.1.7. Engage in "framing," mirroring, or in any other method to imitate the appearance or function of the
-          Services.<br />
-          4.1.8. You may violate any applicable laws or regulations, and encourage or encourage any illegal activity
-          such as trademark infringement copyright violation defamation privacy breach hacking of identities, or the
-          distribution of fake software;<br />
-          4.1.9. You are able to modify or alter the source code of this website. You may also upload applications or
-          software that may cause damage to the Website or another individual.<br />
-          4.1.10. You are not allowed to disassemble, decompile or reverse engineer any technology or software that is
-          on the Website or utilized to provide the Services.<br />
-          4.2. The terms of this agreement are in addition to any rights we might possess. If we believe that Your use
-          of the site is not in compliance with the terms of these Terms, or any other applicable laws, we may be able
-          to monitor your use of the website or the services or block access to it, or reveal Your behavior patterns on
-          the Website to other parties or take any other appropriate measure to safeguard third-party rights and
-          property.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_7') ?></h2>
+        <p><?= t('t.policy.conditions_8') ?></p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">5. Intellectual property rights</h2>
-        <p>
-          5.1. The Website's content, including that is video-related, such as text images, logos, sounds, designs
-          trademarks, and other material are protected by our and third-party intellectual property rights.<br />
-          5.2. All rights, titles and all rights and interests related to the services and website are ours. Other than
-          the rights to utilize the Services and the Website in accordance to these Conditions, the use by the User of
-          the Services and the Website do not grant any intellectual property rights to them.<br />
-          5.3. The Website and/or the Services may only be accessed by the user only for personal, non-commercial
-          use.<br />
-          5.4. You will not permit anyone to modify or reverse engineer, decompile or copy the Services or Website, or
-          make derivative works using the sub-license or rent.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_9') ?></h2>
+        <p><?= t('t.policy.conditions_10') ?></p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">6. Limitation of liability</h2>
-        <p>
-          6.1. The Website and Services are the responsibility of you. Any warranties, whether implied or expressly
-          stated, regarding the Website and Services or Your use of them including implied warranties of quality and
-          merchantability, as well as the purpose for which it was designed like non-infringement and usability,
-          authority in terms of accuracy, completeness, timeliness and prompt delivery are not provided by us. The
-          content and functions accessible or accessible via the website are offered "as is,"" "as accessible," and
-          "with any consequence."<br />
-          6.2. We are not responsible for any mistakes, omissions, or inaccuracies of the information on the website. We
-          do not take responsibility for any disruptions or interruption of transmissions to or through the Services.<br />
-          6.3. We will indemnify You for any loss, directly or indirectly incurred by You or any third party through the
-          website or Services. You will be accountable for any decisions by relying on information on the Website and/or
-          in the Services.<br />
-          6.4. We are not responsible for any loss or damage that directly, indirectly, or related to you or any other
-          third person. This includes any loss of income or data that results from your usage of the site and/or
-          services. This limitation of liability is in the manner allowed by law in the applicable region.<br />
-          6.5. If there's a technical issue with an internet or telephone line or computer provider or system server, or
-          any hardware we do not assume any responsibility. We are not accountable for any usage of the internet.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_11') ?></h2>
+        <p><?= t('t.policy.conditions_12') ?></p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">7. Content and Services from third parties</h2>
-        <p>
-          7.1. You are able to view content from third parties and services when using the Services. This could include
-          ads or reviews of Third-Party Platforms.<br />
-          7.2. We do not take responsibility for the information or products that are referred to. They may not be
-          current or up-to-date at all times.<br />
-          7.3. We suggest that you verify the accuracy of all information prior to making any decision. You are
-          accountable for all decisions and actions from the information.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_13') ?></h2>
+        <p><?= t('t.policy.conditions_14') ?></p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">8. Links</h2>
-        <p>
-          8.1. The site contains advertisements as well as content. The materials are made available through websites of
-          third parties ( "Links" ). We advise you to be aware of this before you download, rely on or access any
-          information obtained from these websites, software or other materials or complete any purchase or other
-          transactions We will advise you. These links are provided for the convenience of users only. We are not
-          accountable for any damage or loss caused by the use or reliance on any information, products or services that
-          are made available through other websites or programs.<br />
-          8.2. The inclusion of hyperlinks on this website does not imply endorsement, authorization or affiliation or
-          any other form of endorsement by Us in connection with the Websites their software or administrators.<br />
-          8.3. We haven't reviewed all of the hyperlinks and cannot be held accountable for the software or website that
-          are referred to. Before you decide to use, rely on or purchase anything from these websites or applications,
-          we warn you. In no way will you be held accountable for any damage or loss caused by the use or reliance on
-          any products, information or content accessible through other websites.<br />
-          8.4. It is your responsibility and obligation to review the terms and policies of any websites operated by
-          third parties. We strongly suggest that you read them prior to making contact with third-party websites.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_15') ?></h2>
+        <p><?= t('t.policy.conditions_16') ?></p>
       </div>
       <div class="grid gap-2 md:gap-4">
-        <h2 class="text-2xl font-medium">9. Miscellaneous</h2>
-        <p>
-          9.1. We are entitled to alter, cease or modify our Services at any given moment. The changes won't cause any
-          harm to You and you will not be able to assert any claims against Us.<br />
-          9.2. The Terms can be modified at anytime. We can change the Terms at anytime. We will inform you by
-          publishing the most current version, and then revising the date on the top. Any modifications will be
-          effective within a couple of working days. You are deemed to accept any modifications to the Terms of the
-          website if you continue to use it after they are released.<br />
-          9.3. The User acknowledges and agree that any information transmitted via or through the website does not
-          create any kind of relationship that is not explicitly stated in these Terms.<br />
-          9.4. These Terms and the Privacy policy, as well as the Privacy policies, as amended from time to time are the
-          sole valid agreements between Us and the User and any other promise, declaration or agreement, whether oral or
-          written, not included in the Privacy Policy is legally binding for the parties.<br />
-          9.5. Inability to exercise any right or power that is provided herein is deemed to be to be a waiver of the
-          right or power. Any one or only a portion of an exercise shall be an element of any additional or further
-          exercise of the same or any other rights or remedies.<br />
-          9.6. These Terms will be cancelled in the event that the provision in question is declared invalid by a court
-          with competent jurisdiction. The remainder of the Terms will be interpreted as in the event that the exclusion
-          was implemented, and will be applied according to their terms. However, the Terms will be interpreted in
-          accordance with the intentions and meanings of the exclusion clauses according to the ruling of the courts.<br />
-          9.7. These Terms permit third-party partners to transfer or assign the entire rights and obligations.
-          Third-party operators are able to manage the website and all of its services, without restricting the
-          previous. These Terms do not permit you to assign or transfer the rights or obligations you have.
-        </p>
+        <h2 class="text-2xl font-medium"><?= t('t.policy.conditions_17') ?></h2>
+        <p><?= t('t.policy.conditions_18') ?></p>
       </div>
     </div>
   </div>
